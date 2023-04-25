2111f1814ced4e9ddd51302deeb3a6fd9d513f25
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2023-04-25 12:24:34 -0700

    Show a random text example in the web app

--- semantic_navigator/app/views.py
+++ semantic_navigator/app/views.py
@@ -7,6 +7,7 @@ from flask import (
     render_template,
 )
 
+from ..data import load_cdp_sea_small
 from . import TEMPLATES_DIR
 
 ###############################################################################
@@ -21,9 +22,18 @@ views = Blueprint(
     template_folder=TEMPLATES_DIR,
 )
 
+CDP_EXAMPLE_DATASET = load_cdp_sea_small()
+
 ###############################################################################
 
 
 @views.route("/", methods=["GET"])
 def index() -> str:
-    return render_template("index.html")
+    random_row_from_dataset = CDP_EXAMPLE_DATASET.sample(1).iloc[0]
+    with open(random_row_from_dataset.chunk_text_path) as open_f:
+        example_random_text = open_f.read()
+
+    return render_template(
+        "index.html",
+        example_random_text=example_random_text,
+    )
