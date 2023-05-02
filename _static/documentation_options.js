af4ef672f9ea5e6586b0050e057f2c7fb919d2a7
evamaxfield <evamaxfieldbrown@gmail.com>
2023-05-02 16:38:05 +0000

    Deploying to gh-pages from @ CouncilDataProject/semantic-navigator@df33027b73b7c861e5116e9559fa4bf4b98ce03c 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g293bba8',
+    VERSION: '0.1.dev1+gdf33027',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
