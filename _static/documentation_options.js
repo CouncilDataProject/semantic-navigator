125aaa1bb28e64be3dde4db9257fa3decb5d222a
ipestovski <isabella@pestovski.com>
2023-05-18 20:52:46 +0000

    Deploying to gh-pages from @ CouncilDataProject/semantic-navigator@c35719f69d571aac5e2115e1b26ebcabdf66ee74 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g9fd8740',
+    VERSION: '0.1.dev1+gc35719f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
